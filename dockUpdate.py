b914abf522303dbbdb50bf458c3175f3849e1587
timcrawf <timcrawf88@gmail.com>
2016-09-02 10:21:12 -0500

    added comments

--- dockUpdate.py
+++ dockUpdate.py
@@ -57,34 +57,34 @@ def main():
         exUserPre=options.excludedUsers                       #Converts string to local variable
         exUser=exUserPre.split(',')                                  #Creates list of string split at ","
 
-    if options.includeParse is None:
-        userList=os.listdir(usersDir)
+    if options.includeParse is None:             #if there are no specified users
+        userList=os.listdir(usersDir)            #retrive list of all users on the computer
         for user in userList:
-            if user.startswith('.'):
-                delList.append(user)
+            if user.startswith('.'):            #looks for hidden folders
+                delList.append(user)            #adds them to the delete list
             for ex in exUser:
                 if ex==user:
-                    delList.append(ex)
+                    delList.append(ex)          #adds exculded users to delete list
         for line in delList:
-            userList.remove(line)
+            userList.remove(line)               #removes all unwated users from user list
     else:
-        userList=[options.includeParse]
+        userList=[options.includeParse]    #if specified makes user list just the spicific users
     
     for user in userList:
-        userNum=pwd.getpwnam(user)[2]
+        userNum=pwd.getpwnam(user)[2]     #gets user id number
         userPath=os.path.join(usersDir, user)
-        prefPath=os.path.join(userPath, 'Library/Preferences/')
-        dockPath=os.path.join(prefPath, 'com.apple.dock.plist')
+        prefPath=os.path.join(userPath, 'Library/Preferences/')   #joins paths for prefrences
+        dockPath=os.path.join(prefPath, 'com.apple.dock.plist')   #joins path for dock plist
         try:
-            os.remove(dockPath)
+            os.remove(dockPath)        #trys to remove dock plist if it exists
         except:
             None
-        shutil.copyfile(newFile, dockPath)
-        os.chown(dockPath, userNum, groupNum)
-        os.chmod(dockPath, 0o600)
-    print 'Compleated'
+        shutil.copyfile(newFile, dockPath)      #copies new dock file
+        os.chown(dockPath, userNum, groupNum)   #changes ownership of dock plist to specified user
+        os.chmod(dockPath, 0o600)               #changes permissions for current user
+    print 'Compleated'                          #lets you know the process is complete
 
-    print userList
+    print userList                      #prints list of all modified users
 
 if __name__ == "__main__":
     main()
